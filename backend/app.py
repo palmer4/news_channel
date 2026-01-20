09644fa396cf7f1d1996d7658c7644f3ce43c3c9
HEIM <167620612+palmer4@users.noreply.github.com>
2026-01-20 19:30:16 +0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -18,7 +18,8 @@ CORS(app, origins=[
     'http://localhost:3000',
     'http://127.0.0.1:3000',
     'http://localhost:5500',
-    'http://127.0.0.1:5500'
+    'http://127.0.0.1:5500',
+    "https://palmer4-world-radio.netlify.app"
 ])
 
 # Configuration
